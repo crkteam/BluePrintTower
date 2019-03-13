f8390fd2e1ff1cda2aab8e10ccbfae4542dac85a
CrkJK <crkjk@CrkJKde-MacBook-Pro.local>
2019-03-13 17:21:03 +0800

    crkJK
    ddd

--- CrkSlide/Assets/Script/BackGround.cs
+++ CrkSlide/Assets/Script/BackGround.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        gameObject.transform.position += new Vector3(-0.01f,0,0);
-        if (gameObject.transform.position.x < -13.5)
-        {
-            gameObject.transform.position += new Vector3(0,0,0);
-        }
-    }
+   
 }
--- CrkSlide/Assets/Script/Pika.cs
+++ CrkSlide/Assets/Script/Pika.cs
@@ -7,6 +7,8 @@ public class Pika : MonoBehaviour
     private int count = 0;
     private Quaternion origin;
 
+    private bool life = true;
+
     private void Start()
     {
         origin = gameObject.transform.rotation;
@@ -15,28 +17,53 @@ public class Pika : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (life)
         {
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0,8);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (transform.position.y > -1)
+                {
+                    count = 0;
+                    CancelInvoke();
+                    gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0, 3.5f);
+                    InvokeRepeating("rotate", 0, 0.01f);
+                }
+                else
+                {
+                    gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0, 3);
+                }
+            }
             
-            CancelInvoke();
-            count = 0;
-            InvokeRepeating("rotate",0,0.01f);
         }
     }
 
     void rotate()
     {
-        
-        transform.Rotate(0,0,-5);
+        transform.Rotate(0, 0, -6);
         count++;
 
-        if (count > 70)
+        if (count > 60)
         {
-            gameObject.transform.rotation = origin;
             count = 0;
             CancelInvoke();
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Block"))
+        {
+            Debug.Log("死亡");
+            life = false;
+            gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(Random.Range(8, -8), 8);
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    // 左71
+    // 正-18
+    // 倒161
+    // 右-108
 }
